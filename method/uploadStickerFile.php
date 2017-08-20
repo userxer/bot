86cf4229f0e033012d426888c3e3b640251f9df1
Mehdi Khodayari <mehdi.khodayari.khoram@gmail.com>
2017-08-20 18:50:19 +0430

    Add files via upload

---
+++ method/uploadStickerFile.php
@@ -0,0 +1,39 @@
+<?php namespace bot\method;
+
+use bot\InputFile;
+use bot\object\Error;
+
+/**
+ * Use this method to upload a .png file with a sticker for later use in
+ * createNewStickerSet and addStickerToSet methods (can be used multiple times).
+ * Returns the uploaded File on success.
+ *
+ * @method true|Error send()
+ * @method bool hasUserId()
+ * @method bool hasPngSticker()
+ * @method $this setUserId($integer)
+ * @method $this setPngSticker($file)
+ * @method $this delUserId()
+ * @method $this delPngSticker()
+ * @method int getUserId($default = null)
+ * @method InputFile getPngSticker($default = null)
+ *
+ * @author Mehdi Khodayari <mehdi.khodayari.khoram@gmail.com>
+ * @since 2.0.1
+ *
+ * Class uploadStickerFile
+ * @package bot\method
+ * @link https://core.telegram.org/bots/api#uploadstickerfile
+ */
+class uploadStickerFile extends Method
+{
+
+    /**
+     * Every method have a response.
+     * @return string the class's name.
+     */
+    protected function response()
+    {
+        return true;
+    }
+}
