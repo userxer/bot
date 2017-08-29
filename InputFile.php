87c94789e58cb2247605f84168f5f61f22a7926e
Mehdi Khodayari <mehdi.khodayari.khoram@gmail.com>
2017-08-30 00:03:36 +0430

    Add files via upload

--- InputFile.php
+++ InputFile.php
@@ -1,8 +1,10 @@
 <?php namespace bot;
 
 /**
- * This object represents the contents of a file to be uploaded. Must be posted using
- * multipart/form-data in the usual way that files are uploaded via the browser.
+ * This object represents the contents of a file to be
+ * uploaded. Must be posted using multipart/form-data in
+ * the usual way that files are uploaded
+ * via the browser.
  *
  * @author Mehdi Khodayari <mehdi.khodayari.khoram@gmail.com>
  * @since 2.0.1
@@ -16,13 +18,13 @@ class InputFile extends \CURLFile
     
     /**
      * InputFile constructor.
-     * @param string $filename
-     * @param string|null $mimetype
-     * @param string|null $postname
+     * @param string $filename path to the file which will be uploaded.
+     * @param string|null $mimetype mimetype of the file.
+     * @param string|null $postname name of the file.
      */
     public function __construct($filename, $mimetype = null, $postname = null)
     {
-        $path = \Yii::getAlias($filename, false);
+        $path = \Yii::getAlias($filename);
         parent::__construct($path, $mimetype, $postname);
     }
 }
